3e80598f9535d13f3d6b3e7458554b2aa3ddee81
dsmundhe <dipakmundhe2026@gmail.com>
2025-03-09 10:57:03 -0700

    oops folder created

--- oops/greet/Greetings.java
+++
@@ -1,7 +0,0 @@
-package oops.greet;
-
-public class Greetings {
-    public static void message(String name){
-        System.out.println("Hello Mrs. " + name );
-    }
-}
--- oops/greet/Main.java
+++
@@ -1,9 +0,0 @@
-package oops.greet;
-
-import static oops.greet.Greetings.message;
-
-public class Main {
-    public static void main(String[] args) {
-        message("Dipak Mundhe");
-    }
-}
