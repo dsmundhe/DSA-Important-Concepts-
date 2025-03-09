9bc25bbe57cf01d5f6335df155165af5dc0e2e91
dsmundhe <dipakmundhe2026@gmail.com>
2025-03-08 22:09:56 -0800

    sum of digits

--- numbers/SumOfDigits.java
+++ numbers/SumOfDigits.java
@@ -8,7 +8,19 @@ public class SumOfDigits {
         System.out.print("Enter Num : ");
         int num=sc.nextInt();
         System.out.println("Sum Of Digits : "+sumOfDigitsFun(num,0));
+        System.out.println("Sum of Digits : "+ sum_of_Digits(num));
     }
+
+
+
+    public static int sum_of_Digits(int num){
+        if(num==0){
+            return 0;
+        }
+        return (num %10 + sum_of_Digits(num /10));
+    }
+
+
     public static int sumOfDigitsFun(int num,int sum){
         if(num ==0){
             return sum;
