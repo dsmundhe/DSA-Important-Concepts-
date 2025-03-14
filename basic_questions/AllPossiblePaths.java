0bdbc5ec9c2de63f1c903a03d554ffbc57297c80
dsmundhe <dipakmundhe2026@gmail.com>
2025-03-14 06:03:31 -0700

    distinct paths

--- basic_questions/AllPossiblePaths.java
+++ basic_questions/AllPossiblePaths.java
@@ -1,27 +1,62 @@
 package basic_questions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AllPossiblePaths {
     public static void main(String[] args) {
-        int[][] grid={
-                {0 ,2 ,3},
-                {1 ,3 ,2},
-                {1 ,1, 0}
-        };
-        System.out.println(moneyFun(grid,0,0,0));
+        int[][] grid = {
+                {0, 2, 3},
+                {1, 3, 2},
+                {1, 1, 0}};
+        pathsFun(grid,0,0,0);
+        System.out.println("List : "+pathsFunList(grid,0,0,0));
+        List<Integer> ls=pathsFunList(grid,0,0,0);
+        ArrayList<Integer> newList=new ArrayList<>();
+        int count=0;
+        int sum=0;
+        for (Integer val:ls){
+            if(!newList.contains(val)){
+                count++;
+                sum+=val;
+                newList.add(val);
+            }
+        }
+        System.out.println("Sum : "+sum);
+        System.out.println("Total distinct paths : "+count);
     }
 
-    public static int moneyFun(int[][] arr,int i,int j,int sum){
-       sum+=arr[i][j];
-        if(i==arr.length-1 && j== arr[0].length-1){
-            return sum;
+    public static List<Integer> pathsFunList(int[][] arr,int row,int col,int sum){
+        if(row==arr.length-1 && col==arr.length-1){
+
+            List<Integer> ls=new ArrayList<>();
+            ls.add(sum+arr[row][col]);
+            return ls;
+        }
+        ArrayList<Integer> list=new ArrayList<>();
+        if(row<arr.length-1){
+            list.addAll(pathsFunList(arr,row+1,col,sum+arr[row][col]));
+        }
+        if(col<arr.length-1){
+            list.addAll(pathsFunList(arr,row,col+1,sum+arr[row][col]));
+        }
+        return list;
+    }
+
+
+    public static void pathsFun(int[][] arr,int row,int col,int sum){
+        if(row==arr.length-1 && col==arr.length-1){
+            System.out.println(sum+arr[row][col]);
+            return;
         }
 
-        if(i+1<arr.length){
-            moneyFun(arr,i+1,j,sum);
+        if(row<arr.length-1){
+            pathsFun(arr,row+1,col,sum+arr[row][col]);
         }
-        if(j+1<arr.length){
-            moneyFun(arr,i,j+1,sum);
+        if(col< arr.length-1){
+             pathsFun(arr,row,col+1,sum+arr[row][col]);
         }
-        return sum;
     }
+
 }
+
