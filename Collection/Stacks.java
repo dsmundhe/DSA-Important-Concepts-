7e9248b4b44849618d9694de7dbbaaeeeb3c367b
dsmundhe <dipakmundhe2026@gmail.com>
2025-03-19 07:07:40 -0700

    Stack

---
+++ Collection/Stacks.java
@@ -0,0 +1,38 @@
+package Collection;
+import java.util.Stack;
+
+public class Stacks {
+    public static void main(String[] args) {
+
+//  A stack is linear data structure works on LIFO -> (Last in first out)
+//  values stored like stack-plates
+//  can perform push and pop operation
+
+//        Stack Operation
+//        push -> stack.push(value); add at the top
+//        pop -> stack.pop(); remove and return top element
+//        isEmpty -> stack.isEmpty(); check weather stack empty
+//        peek -> stack.peek() ; get top element without removing
+//        search -> stack.search(value); get the position of element
+
+        Stack<Integer> stack=new Stack<>();
+//      push value
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+        System.out.println("Stack : "+stack);
+
+//      pop value
+        System.out.println("Before poped : "+stack);
+        System.out.println("Popped Element : "+ stack.pop());
+        System.out.println("After poped : "+stack);
+
+//       peek value
+        System.out.println("Peek Element : "+ stack.peek());
+
+//      isEmpty
+        System.out.println("Is stack Empty : "+stack.isEmpty());
+    }
+}
