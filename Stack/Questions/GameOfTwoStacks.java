d43afc79cd501bb3ff757ea2240d603931b68899
dsmundhe <dipakmundhe2026@gmail.com>
2025-04-06 11:37:57 -0700

    game of two stacks optimized way

--- Stack/Questions/GameOfTwoStacks.java
+++ Stack/Questions/GameOfTwoStacks.java
@@ -1,5 +1,6 @@
 package Stack.Questions;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class GameOfTwoStacks {
@@ -20,11 +21,29 @@ public class GameOfTwoStacks {
         int maxSum=10;
         System.out.println(countFun(stackA,stackB,maxSum));
 
-
+        int[] arrA={4,2,4,6,1};
+        int[] arrB={2,1,8,5};
+        System.out.println("Count : "+gameOfTwo(arrA,arrB,maxSum));
 
     }
     //Optimized and Solve Edges cases
    //using arrays
+public static int gameOfTwo(int[] arrA,int[] arrB,int max){
+        return gameTwo(arrA,arrB,max,0,0)-1;
+}
+
+public static int gameTwo(int[] arrA,int[] arrB,int max,int sum,int count){
+        if(sum>max){
+            return count;
+        }
+        if(arrA.length==0|| arrB.length==0){
+            return count;
+        }
+        int num1=gameTwo(Arrays.copyOfRange(arrA,1,arrA.length),arrB,max, sum+arrA[0],count+1);
+        int num2=gameTwo(arrA,Arrays.copyOfRange(arrB,1,arrB.length),max,sum+arrB[0],count+1);
+
+        return Math.max(num1,num2);
+}
 
     //not optimized and not solve edge cases
     public static int countFun(Stack<Integer> A,Stack<Integer>B,int maxSum){
